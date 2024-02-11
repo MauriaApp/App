64d42f6f598a015db99fb16d109ccb19387a2939
Mylow <milo.montuori@student.junia.com>
2024-02-12 00:00:01 +0100

    up WelcomeMsg

--- src/pages/Home/WelcomeModalContent/index.tsx
+++ src/pages/Home/WelcomeModalContent/index.tsx
@@ -10,46 +10,45 @@ const WelcomeModalContent = () => {
     <>
       <div className={styles["headerModal"]}>
         <h2 className={"sectionTitle text-primary no-margins"}>
-          Bienvenue sur Mauria !
+          Bienvenue sur Mauria (version web) !
         </h2>
       </div>
       <p className={styles["modalText"]}>
         <strong style={{ color: "#f17853" }}>Salut !</strong> On a remarqué que
-        c'était ta première fois sur l'application. On te propose de t'expliquer
+        c'était ta première fois sur le site. On te propose de t'expliquer
         comment ça marche et comment t'y retrouver.
         <br />
         <br />
         <strong style={{ color: "#f17853" }}>
-          Mauria ne remplace pas Aurion ! 
-        </strong> Mauria est un outil complémentaire à Aurion, il ne remplace pas le site et ses données.
+          Mauria ne remplace pas Aurion !
+        </strong> C'est un outil complémentaire à Aurion, il ne remplace pas le site et ses données.
         <br />
         <br />
-        L'application est divisée en plusieurs onglets, tu peux les retrouver en
+        Le site est divisé en plusieurs onglets, tu peux les retrouver en
         bas de l'écran. Il y a aussi un menu en haut à droite avec des options
         supplémentaires.
         <br />
         <br />
-        L'application n'enregistre rien, tout est stocké sur ton téléphone. De
-        ce fait, tu peux l'utiliser sans connexion internet ! (Mais tu ne
-        pourras pas actualiser les données)
+        Le site n'enregistre rien, tout est stocké sur ton navigateur.
+        Donc, si tu changes de navigateur, tu n'auras plus rien. Pareil si tu es en navigation privée.
         <br />
-        Les notes et absences{" "}
-        <strong style={{ color: "#f17853" }}>
-          ne sont pas mise à jour automatiquement
-        </strong>
-        , il faut actualiser manuellement pour les voir : il suffit de faire un
-        geste vers le bas sur la page (un swipe)
         <br />
-        Le planning est mis à jour automatiquement toutes les 6 heures (en cours
-        de test, donc{" "}
         <strong style={{ color: "#f17853" }}>
-          on te conseille de le faire manuellement au moins une fois par jour
+          RIEN<span> </span>
         </strong>
-        ).
+        n'est mis à jour automatiquement, il faut donc cliquer sur le bouton "Actualiser"
+        disponible dans chaque onglet pour mettre à jour les données.
+        <br />
+        <strong style={{ color: "#f17853" }}>
+          Pitié actualises de temps en temps... Ça prend quelques secondes et ça évite les problèmes.
+        </strong>
         <br />
         <br />
         Si tu as des questions, n'hésite pas à nous contacter dans l'onglet
         "Support" !
+        <br />
+        <br />
+        Tout le code de Mauria est disponible sur   https://github.com/MauriaApp
         <br />
         <br />
         Par Milo Montuori et Louis Lecouturier 👍
